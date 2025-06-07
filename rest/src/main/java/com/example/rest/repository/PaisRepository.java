5e931c5af6f7c496888ce4b2a1000c74c355dbbd
JuanPE-PNG <juan.escobar.viveros@correounivalle.edu.co>
2025-06-07 15:22:20 -0500

    Segundo codelab

---
+++ rest/src/main/java/com/example/rest/repository/PaisRepository.java
@@ -0,0 +1,12 @@
+package com.example.rest.repository;
+
+import com.example.rest.model.Pais;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PaisRepository extends JpaRepository<Pais, Long> {
+    Optional<Pais> findByNombre(String nombre);
+}
